659575187529faed4d17e99699390c4a25ff3366
Pedro Gamboa <pgamboa@elcentrochc.org>
2024-01-10 18:34:22 -0600

    add install functionality

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,10 +2,26 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    window.differedPrompt = event;
+    butInstall.classList.toggle('hidden', false);
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+});
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+// Implemented a click event handler on the `butInstall` element
+butInstall.addEventListener('click', async () => {
+    const { differedPrompt } = window;
+    if (!differedPrompt) {
+        return;
+    }
+    differedPrompt.prompt();
+    const { outcome } = await differedPrompt.userChoice;
+    console.log(`User response to the install prompt: ${outcome}`);
+    window.differedPrompt = null;
+    butInstall.classList.toggle('hidden', true);
+});
+
+// Added an handler for the `appinstalled` event
+window.addEventListener('appinstalled', (event) => {
+    window.differedPrompt = null;
+});
